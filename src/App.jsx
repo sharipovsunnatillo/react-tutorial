1cc772e9d3b2fa75e32c4da6672bf0db69c06e72
sunnatillo <ssharipov@cdti.uz>
2025-03-20 14:56:16 +0500

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
-import Concept from "./components/Concept/Concept.jsx";
-import "./app.css"
-import reactLogo from "./assets/react.svg";
-
 function App() {
-    const concepts = [
-        {
-
-            title: "Components",
-            description: "Lorem ipsum dolor sit amet, consectetur nostrum officiis, optio perferendis porro, provident quae sed sunt ut vero?",
-            imageUrl: reactLogo
-        },
-        {
-            title: "JSX",
-            description: "Lorem ipsum dolor sit amet, consectetur nostrum officiis, optio perferendis porro, provident quae sed sunt ut vero?",
-            imageUrl: reactLogo
-        },
-        {
-            title: "Props",
-            description: "Lorem ipsum dolor sit amet, consectetur nostrum officiis, optio perferendis porro, provident quae sed sunt ut vero?",
-            imageUrl: reactLogo
-        },
-        {
-            title: "State",
-            description: "Lorem ipsum dolor sit amet, consectetur nostrum officiis, optio perferendis porro, provident quae sed sunt ut vero?",
-            imageUrl: reactLogo
-        },
-    ]
-    return <div className="main">
-        <h1>Tutorial</h1>
-        <div className="concept-wrapper">
-            {concepts.map(concept => (
-                <Concept title={concept.title} description={concept.description} imageUrl={concept.imageUrl}/>
-            ))}
+    return <div className="container-fluid p-5 text-light">
+        <h1 className="display-5 text-center font-monospace">Components</h1>
+        <div className="container text-center">
+            <div className="row align-items-start">
+                <div className="col"><img src="/1.png" className="img-thumbnail p-4 mb-2" alt="image"/>
+                    <h2>Header 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus, nam. Expedita in
+                        nemo quae quasi, rerum vel! Itaque, repellat.</p>
+                </div>
+                <div className="col"><img src="/2.png" className="img-thumbnail p-4 mb-2" alt="image"/>
+                    <h2>Header 2</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis debitis est, illo
+                        magni odit pariatur quidem sit ullam unde!</p>
+                </div>
+                <div className="col"><img src="/3.png" className="img-thumbnail p-4 mb-2" alt="image"/>
+                    <h2>Header 3</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ea neque porro provident quia
+                        sed tenetur ut voluptate. Alias, placeat.</p>
+                </div>
+                <div className="col"><img src="/4.png" className="img-thumbnail p-4 mb-2" alt="image"/>
+                    <h2>Header 4</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci beatae delectus eum hic ipsa
+                        nobis omnis perferendis quas totam veritatis.</p>
+                </div>
+            </div>
+        </div>
+        <div className="container text-center">
+            <h2 className="display-5 text-center font-monospace mt-5">Examples</h2>
+            <ul className="nav nav-underline">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <p className="lead text-start mt-3">Content</p>
         </div>
     </div>
 }
--- src/components/Concept/Concept.jsx
+++
@@ -1,9 +0,0 @@
-import "./concept.css"
-
-export default function Concept({title, imageUrl, description}) {
-    return <div className="concept">
-        <img src={imageUrl} alt="image"/>
-        <h2>{title}</h2>
-        <p>{description}</p>
-    </div>
-}
