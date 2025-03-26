3a4c867473b132a9d0763628fd9496dcc1bcb199
sunnatillo <ssharipov@cdti.uz>
2025-03-26 14:22:56 +0500

    tic tac toe board

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
 function App() {
-    return <div className="container-fluid p-5 text-light">
-        <h1 className="display-5 text-center font-monospace">Components</h1>
-        <div className="container text-center">
-            <div className="row align-items-start">
-                <div className="col"><img src="/1.png" className="img-thumbnail p-4 mb-2" alt="image"/>
-                    <h2>Header 1</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus, nam. Expedita in
-                        nemo quae quasi, rerum vel! Itaque, repellat.</p>
-                </div>
-                <div className="col"><img src="/2.png" className="img-thumbnail p-4 mb-2" alt="image"/>
-                    <h2>Header 2</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis debitis est, illo
-                        magni odit pariatur quidem sit ullam unde!</p>
-                </div>
-                <div className="col"><img src="/3.png" className="img-thumbnail p-4 mb-2" alt="image"/>
-                    <h2>Header 3</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ea neque porro provident quia
-                        sed tenetur ut voluptate. Alias, placeat.</p>
-                </div>
-                <div className="col"><img src="/4.png" className="img-thumbnail p-4 mb-2" alt="image"/>
-                    <h2>Header 4</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci beatae delectus eum hic ipsa
-                        nobis omnis perferendis quas totam veritatis.</p>
-                </div>
+    return <main className="container">
+        <div className="players">
+            <div className="player">
+                <span className="name">Player 1</span>
+                <button className="btn">Edit</button>
+            </div>
+            <div className="player">
+                <span className="name">Player 1</span>
+                <button className="btn">Edit</button>
             </div>
         </div>
-        <div className="container text-center">
-            <h2 className="display-5 text-center font-monospace mt-5">Examples</h2>
-            <ul className="nav nav-underline">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <p className="lead text-start mt-3">Content</p>
+        <div className="game-board">
+            <div className="row">
+                <div className="cell"><span className="symbol">0</span></div>
+                <div className="cell"><span className="symbol">0</span></div>
+                <div className="cell"><span className="symbol">0</span></div>
+            </div>
+            <div className="row">
+                <div className="cell"><span className="symbol">0</span></div>
+                <div className="cell"><span className="symbol">0</span></div>
+                <div className="cell"><span className="symbol">0</span></div>
+            </div>
+            <div className="row">
+                <div className="cell"><span className="symbol">0</span></div>
+                <div className="cell"><span className="symbol">0</span></div>
+                <div className="cell"><span className="symbol">0</span></div>
+            </div>
         </div>
-    </div>
+    </main>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "popper.js/dist/popper.min.js"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "./styles.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
